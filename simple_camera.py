743850446b13d40bc1915053f5198608e6d6c66b
karaage0703 <coldchicken@gmail.com>
2023-03-13 10:55:25 +0900

    Initial commit

---
+++ simple_camera.py
@@ -0,0 +1,13 @@
+import cv2
+cap = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        break
+
+    key = cv2.waitKey(1)
+    if key == 27:  # when ESC key is pressed break
+        break
+
+    cv2.imshow('camera', frame)
